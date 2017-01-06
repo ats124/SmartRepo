0ebd45650acc49f9ed717cd6030c952d155525a8
Atsushi Tanaka <ats124@section-9.tech>
2017-01-07 01:18:41 +0900

    アプリをPrism.Formsに対応

---
+++ SmartRepo/SmartRepo/App.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Prism.Unity;
+
+namespace Softentertainer.SmartRepo
+{
+    using Views;
+
+    public class App : PrismApplication
+    {
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<InputReportPage>();
+        }
+
+        protected override void OnInitialized()
+        {
+            NavigationService.NavigateAsync(nameof(InputReportPage));
+        }
+    }
+}
