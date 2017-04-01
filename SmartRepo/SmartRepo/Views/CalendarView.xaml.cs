cb711b0d93106b6b8dbb725f280645a037c57009
Atsushi Tanaka <ats124@section-9.tech>
2017-04-01 21:26:27 +0900

    カレンダーの当月表示

--- SmartRepo/SmartRepo/Views/CalendarView.xaml.cs
+++ SmartRepo/SmartRepo/Views/CalendarView.xaml.cs
@@ -10,18 +10,99 @@ namespace Softentertainer.SmartRepo.Views
 {
     public partial class CalendarView : ContentView
     {
+        public static readonly BindableProperty CalendarItemsSourceProperty =
+            BindableProperty.Create(nameof(CalendarItemsSource), typeof(IDictionary<DateTime, object>), typeof(CalendarView),
+                propertyChanged: (bindable, oldValue, newValue) => ((CalendarView)bindable).RefreshCalendarCells(isRefreshSource: true));
+        /// <summary>
+        /// カレンダーのデータを取得または設定します。
+        /// </summary>
+        public IDictionary<DateTime, object> CalendarItemsSource
+        {
+            get => (IDictionary<DateTime, object>)this.GetValue(CalendarItemsSourceProperty);
+            set => this.SetValue(CalendarItemsSourceProperty, value);
+        }
+
+        public static readonly BindableProperty CalendarItemTemplateProperty =
+             BindableProperty.Create(nameof(CalendarItemTemplate), typeof(DataTemplate), typeof(CalendarView),
+                propertyChanged: (bindable, oldValue, newValue) => ((CalendarView)bindable).RefreshCalendarCells(isRefreshTemplate: true));
+        /// <summary>
+        /// カレンダーデータを表現するテンプレートを取得または設定します。
+        /// </summary>
+        public DataTemplate CalendarItemTemplate
+        {
+            get => (DataTemplate)this.GetValue(CalendarItemTemplateProperty);
+            set => this.SetValue(CalendarItemTemplateProperty, value);
+        }
+
+        public static readonly BindableProperty ViewMonthProperty =
+            BindableProperty.Create(nameof(ViewMonth), typeof(DateTime), typeof(CalendarView), DateTime.MinValue,
+                defaultValueCreator: bindable =>
+                {
+                    var today = DateTime.Today;
+                    return new DateTime(today.Year, today.Month, 1);
+                },
+                propertyChanged: (bindable, oldValue, newValue) => ((CalendarView)bindable).RefreshCalendarCells(isRefreshDay: true));
+        /// <summary>
+        /// カレンダー表示する年月を取得または設定します。
+        /// </summary>
+        public DateTime ViewMonth
+        {
+            get => (DateTime)this.GetValue(ViewMonthProperty);
+            set => this.SetValue(ViewMonthProperty, new DateTime(value.Year, value.Month, 1));
+        }
+
+        private readonly Color CalendarBackgroundColor;
+        private readonly Color SundayColor;
+        private readonly Color SaturdayColor;
+        private readonly Color OutOfRangeDayColor;
+        private readonly string[] DayOfWeeks;
+
+        private readonly List<(Label DayLabel, ContentView DayContent)> dayElements;
+
         public CalendarView()
         {
             InitializeComponent();
 
-            // 日付部分のグリッド定義 7日 * 5週
-            for (var i = 0; i < 5; i++)
+            this.CalendarBackgroundColor = (Color)this.Resources["CalendarBackgroundColor"];
+            this.SundayColor = (Color)this.Resources["SundayColor"];
+            this.SaturdayColor = (Color)this.Resources["SaturdayColor"];
+            this.OutOfRangeDayColor = (Color)this.Resources["OutOfRangeDayColor"];
+            this.DayOfWeeks = (string[])this.Resources["DayOfWeeks"];
+
+            // 曜日行
+            CalendarDaysGrid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
+            // 6週
+            for (var i = 0; i < 6; i++)
                 CalendarDaysGrid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Star });
+            // 曜日列
             for (var i = 0; i < 7; i++)
                 CalendarDaysGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Star });
 
+            // 曜日のラベル
+            this.DayOfWeeks.ForEach((w, index) =>
+            {
+                var dayOfWeek = (DayOfWeek)index;
+                var label = new Label()
+                {
+                    Text = w,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    BackgroundColor = this.CalendarBackgroundColor,
+                    HorizontalOptions = LayoutOptions.Fill,
+                    TextColor = 
+                        dayOfWeek == DayOfWeek.Sunday 
+                            ? this.SundayColor
+                        : dayOfWeek == DayOfWeek.Saturday 
+                            ? this.SaturdayColor
+                            : Color.Default,
+                };
+                Grid.SetRow(label, 0);
+                Grid.SetColumn(label, index);
+                CalendarDaysGrid.Children.Add(label);
+            });
+
             // 日付グリッド内に日付ラベル
-            Enumerable.Range(0, 5).ForEach(row =>
+            this.dayElements = new List<(Label DayLabel, ContentView DayContent)>();
+            Enumerable.Range(0, 6).ForEach(row =>
                 Enumerable.Range(0, 7).ForEach(col =>
                 {
                     var grid = new Grid()
@@ -33,18 +114,64 @@ namespace Softentertainer.SmartRepo.Views
                             new RowDefinition() { Height = GridLength.Auto },
                             new RowDefinition() { Height = GridLength.Star },
                         },
-                        BackgroundColor = (Color)this.Resources["CalendarBackgroundColor"],
+                        BackgroundColor = this.CalendarBackgroundColor,
                     };
-                    Grid.SetRow(grid, row);
+                    Grid.SetRow(grid, row + 1);
                     Grid.SetColumn(grid, col);
+                    CalendarDaysGrid.Children.Add(grid);
 
-                    var label = new Label() { Text = $"{col + 7 * row + 1}" };
+                    // 日を表すラベル
+                    var label = new Label();
                     label.HorizontalOptions = LayoutOptions.End;
                     Grid.SetRow(label, 0);
                     grid.Children.Add(label);
 
-                    CalendarDaysGrid.Children.Add(grid);
+                    // カレンダーのセルに載せるコンテンツ
+                    var content = new ContentView();
+                    Grid.SetRow(content, 1);
+                    grid.Children.Add(content);
+
+                    this.dayElements.Add((label, content));
                 }));
+
+            RefreshCalendarCells(true, true, true);
+        }
+
+        private void RefreshCalendarCells(bool isRefreshDay = false, bool isRefreshTemplate = false, bool isRefreshSource = false)
+        {
+            // カレンダーの先頭日付(日曜日から始める)を取得する
+            var viewMonth = this.ViewMonth;
+            var firstCalendarDay = viewMonth.AddDays(-(int)viewMonth.DayOfWeek);
+
+            this.dayElements.ForEach((dayElement, index) =>
+            {
+                var date = firstCalendarDay.AddDays(index);
+
+                // 日付
+                if (isRefreshDay)
+                {
+                    dayElement.DayLabel.Text = date.ToString("%d");
+                    dayElement.DayLabel.TextColor = date.Month == viewMonth.Month
+                        ? Color.Default
+                        : this.OutOfRangeDayColor;
+                }
+
+                // コンテンツのテンプレート
+                if (isRefreshTemplate)
+                {
+                    dayElement.DayContent.Content = this.CalendarItemTemplate?.CreateContent() as View;
+                }
+
+                // データバインド
+                if (isRefreshSource)
+                {
+                    dayElement.DayContent.BindingContext =
+                        this.CalendarItemsSource != null &&
+                        this.CalendarItemsSource.TryGetValue(date, out var data)
+                            ? data
+                            : null;
+                }
+            });
         }
     }
 }
