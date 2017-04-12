be52e9ca2f026bc42b4968ca895f34ce30d2ed74
tinycomet <little6170@gmail.com>
2017-04-12 23:48:57 +0900

    作業時刻のUIを変更

---
+++ SmartRepo/SmartRepo.iOS/NoBorderEffect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+[assembly: ResolutionGroupName("SmartRepo")]
+[assembly: ExportEffect(typeof(Softentertainer.SmartRepo.iOS.NoBorderEffect), "NoBorderEffect")]
+
+namespace Softentertainer.SmartRepo.iOS
+{
+	public class NoBorderEffect : PlatformEffect
+	{
+		public NoBorderEffect()
+		{
+		}
+
+		protected override void OnAttached()
+		{
+			var uiTxtFieald = (UITextField)Control;
+			uiTxtFieald.BorderStyle = UITextBorderStyle.None;
+		}
+
+		protected override void OnDetached()
+		{
+			// throw new NotImplementedException();
+		}
+	}
+}
---
+++ SmartRepo/SmartRepo/Effects/NoBorderEffect.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xamarin.Forms;
+namespace Softentertainer.SmartRepo
+{
+	public class NoBorderEffect : RoutingEffect
+	{
+		public NoBorderEffect() : base("SmartRepo.NoBorderEffect")
+		{
+		}
+	}
+}
